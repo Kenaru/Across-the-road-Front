74d8e2190d757b8e1c2d99d978eaed9847aeb3f9
Aurélien Bizien <aurelienbizien@outlook.com>
2023-11-10 14:22:08 +0100

    Update post.controller.js connection database

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,21 +1,20 @@
 const db = require('../config/db');
 
 exports.login_user = async (req, res) => {
-        const { mail, password } = req.body;
-        try {
-          const [rows] = await connection.execute(
-            'SELECT * FROM Users WHERE Mail = ? AND Password = ?',
-            [mail, password]
-          );
-          if (rows.length > 0) {
+    const { mail, password } = req.body;
+
+    try {
+        const [rows] = await db.execute('SELECT * FROM Users WHERE Mail = ? AND Password = ?', [mail, password]);
+
+        if (rows.length > 0) {
             // L'utilisateur existe, les détails sont corrects
             res.status(200).json({ success: true, message: 'Login successful' });
-          } else {
+        } else {
             // Aucun utilisateur trouvé avec les détails fournis
             res.status(401).json({ success: false, message: 'Invalid username or password' });
-          }
-        } catch (error) {
-          console.error('Error executing MySQL query:', error);
-          res.status(500).json({ success: false, message: 'Internal Server Error' });
         }
-    };
+    } catch (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+};
