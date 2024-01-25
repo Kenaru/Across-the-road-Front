48b2477e0d6914dd5b90a4901c58f5a975cacebd
Aurélien Bizien <aurelienbizien@outlook.com>
2024-01-25 13:39:32 +0100

    Init Frontend

--- old/src/acrosstheroad/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from datetime import datetime
-
-def index(request):
-    return render(request, 'acrosstheroad/index.html', context={"date": datetime.now()})
