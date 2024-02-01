5f276731f40ca0b2049e63f5ef9fb3889d2051bd
Jalkhmo <ponthieujade@gmail.com>
2024-02-01 15:49:14 +0100

    petites modifs

---
+++ src/components/ResetPassword.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import './styles/styles.module.css';
+
+
+const ResetPassword = () => {
+    return (
+      <div className={ResetPasswordStyles.container}>
+        <div className={ResetPasswordStyles.leftPanel}>
+          <form id="reset-password-form" action="" method="post">
+            <h1>Réinitialisation du mot de passe</h1>
+            <input type="password" id="password" name="password" placeholder="Nouveau mot de passe" required />
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirmer le nouveau mot de passe"
+              required/>
+            <br />
+            <a href="../login/login.html" id="forgot-password">Se connecter</a>
+            <br />
+            <div id="response"></div>
+            <button type="submit">Envoyer</button>
+            <br />
+          </form>
+        </div>
+        <Image
+          src="../../../img/logo_typo_noire.svg"
+          alt="logo"
+          width={width} // Spécifiez la largeur souhaitée
+          height={height} // Spécifiez la hauteur souhaitée
+        />
+      </div>
+    );
+  };
+
+export default {ResetPassword}
